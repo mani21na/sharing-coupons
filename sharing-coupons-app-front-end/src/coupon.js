4ff8ccf446e1af0ac750e8218242c79c8cef5b20
mani21na <mani21na@gmail.com>
2020-03-16 12:08:36 -0700

    first commit

---
+++ sharing-coupons-app-front-end/src/coupon.js
@@ -0,0 +1,44 @@
+class Coupon{
+
+    static all = []
+  
+    constructor({id, code, offerType, description, expirationDate, storeId}){
+      this.id = id;
+      this.code = code;
+      this.offerType = offerType;
+      this.description = description;
+      this.expirationDate = expirationDate;
+      this.storeId = storeId
+  
+      this.element = document.createElement('div')
+      this.element.className = "coupon"
+      this.element.id = `coupon-${this.id}`
+  
+      Coupon.all.push(this)
+    }
+  
+    store(){
+      return Store.all.find(store => store.id === this.storeId)
+    }
+  
+    partialRender(){
+      this.element.innerHTML = `
+        <h3>${this.code}</h3>
+        <p>Store: ${this.store().name}</p>
+        <p>Description: ${this.description}</p>
+      `
+      return this.element
+    }
+  
+  
+    fullRender(){
+      this.element.innerHTML = `
+      <h1>${this.code}</h1>
+      <p>Store: ${this.store().name}</p>
+      <p>Offer Type: ${this.offerType}</p>
+      <p>Description: ${this.description}</p>
+      <p>Expiration Date: ${this.expirationDate}</p>
+      `
+      return this.element
+    }
+  }
---
+++ sharing-coupons-app-front-end/src/index.js
@@ -0,0 +1,98 @@
+//Get all Coupons
+//Get all Stores
+
+//server request
+const couponsAdapter = new CouponsAdapter("http://127.0.0.1:3000/coupons")
+//const StoresAdapter = new StoresAdapter("http://localhost:3000/stores")
+
+couponsAdapter.fetchCoupons()
+//StoresAdapter.fetchStores()
+
+const main = document.getElementById('main')
+const menu = document.getElementById('menu')
+
+const formDiv = document.createElement('div')
+
+menu.addEventListener('click', handleMenuClick)
+formDiv.addEventListener('click', handleFormSubmit)
+
+function handleMenuClick (event){
+  if (event.target.id !== menu){
+    main.innerHTML = ``
+    callbacks[`${event.target.id}`]()
+  }
+}
+
+function handleFormSubmit(event){
+  if(event.target.tagName == "BUTTON"){
+    let inputs = formDiv.querySelectorAll('input')
+    let select = formDiv.querySelector('select')
+    let newCouponObj = {
+      code: inputs[0].value,
+      offer_type: inputs[1].value,
+      description: inputs[2].value,
+      expiration_date: inputs[3].value,
+      storeId: select.value
+    }
+    couponsAdapter.newCoupon(newCouponObj)
+  }
+}
+
+const callbacks = {
+  allCoupons: renderAllCoupons,
+  couponsStores: renderAllCouponsStores,
+  newCoupon: renderNewCouponForm,
+  //newStore: renderNewStoreForm
+}
+
+function renderAllCoupons(){
+  Coupon.all.forEach(coupon => {
+    main.appendChild(coupon.fullRender())
+  })
+}
+
+function renderAllCouponsStores(){
+  Store.all.forEach(store => {
+    main.appendChild(store.fullRender())
+  })
+}
+
+function renderNewCouponForm(){
+  formDiv.innerHTML = `
+    Coupon Code:
+    <input type="text" />
+    <br>
+    Offer Type:
+    <select>
+      <option value="online">Online Code</option>
+      <option value="store">In-Store Coupon</option>
+    </select>
+    <br>
+    Coupon Description:
+    <input type="text" />
+    <br>
+    <select>
+       <option value="default" selected="selected">Select Store of this Coupon </option>
+     ${Store.all.map(store => {
+       return `<option value=${store.id}>${store.name}</option>`
+     }).join("")}
+    </select>
+    <button type="button">New Coupon</button>
+  `
+  main.appendChild(formDiv)
+}
+
+//function renderNewStoreForm() {
+//  formDiv.innerHTML = `
+//    Store:
+//    <input type="text" />
+//    <br>
+//    Website:
+//    <input type="text" />
+//    <br>
+//    <button>New Store</button>
+//  `
+//  main.appendChild(formDiv)
+//}
+
+
