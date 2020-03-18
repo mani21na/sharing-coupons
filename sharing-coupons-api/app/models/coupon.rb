f3924f66b5f98755fdd7e887c49b044b91e23087
mani21na <mani21na@gmail.com>
2020-03-17 23:55:11 -0700

    fix attribute name

--- sharing-coupons-api/app/models/coupon.rb
+++ sharing-coupons-api/app/models/coupon.rb
@@ -2,5 +2,5 @@ class Coupon < ApplicationRecord
     belongs_to :store
  
     validates :code, :offer_type, :description, presence: true
-    validates :username, uniqueness: true
+    validates :code, uniqueness: true
 end
