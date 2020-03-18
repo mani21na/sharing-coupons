02562b66f6cb9061df209f92202500f9aaae51ca
mani21na <mani21na@gmail.com>
2020-03-18 06:17:12 -0700

    add delete method

--- sharing-coupons-app-front-end/src/couponsAdapter.js
+++ sharing-coupons-app-front-end/src/couponsAdapter.js
@@ -31,4 +31,24 @@ class CouponsAdapter {
           console.log(error.message);
         });
     }
+
+    delCoupon(objId) {
+      let configObj = {
+        method: "DELETE",
+        headers: {"Access-Control-Request-Headers": "origin, x-requested-with", "Content-Type": "application/json", "Accepts": "application/json"},
+      }
+      let id = objId
+
+      fetch(`${this.baseURL}/${objId}`, configObj)        
+        .then(function(){
+          console.log("delete!")
+        .catch (error => console.log(error))
+      });
+
+      this.arrayRemove(Coupon.all, id)
+    }
+
+    arrayRemove(array, value) { 
+      return array.filter(e => e.id !== value);
+    }
   }
